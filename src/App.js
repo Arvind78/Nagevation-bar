419abb6e8e6e4d89112bf17a1de898d4b40f1a67
Arvind78 <av894883@gmail.com>
2022-09-02 17:05:26 +0530

    4 files modified, 7 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./style.css";
-
+import Nav from "./Navbar"
+import Home from "./Home"
+import About from  "./About"
+import Login from "./Login"
+import Signup from "./Signup"
+import Pagenot from "./Pagenot"
+ 
+import {Routes ,Route}  from "react-router-dom"
 export default function App() {
   return (
     <div>
- 
+    <Nav/>
+    <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="singup" element={<Signup/>}></Route>
+    <Route path="about" element={<About/>}></Route>
+    <Route path="login" element={<Login/>}></Route>
+    <Route path="*" element={<Pagenot/>}></Route>
+      
+      </Routes>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css"
+import {NavLink} from "react-router-dom"
+const Nav =()=>{
+  return (
+  <>
+  <header className="header">
+  <NavLink to ="/">Home</NavLink>
+  <NavLink to ="/about">About</NavLink>
+  <NavLink to ="/login">Login</NavLink>
+  <NavLink to ="/singup">Signup</NavLink>
+   
+</header>
+    </>
+  )
+}
+export default Nav;
