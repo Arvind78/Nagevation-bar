2cda14f1683c34af5fb2d59881738f721ae696b3
Arvind78 <av894883@gmail.com>
2022-09-02 17:46:28 +0530

    2 files modified

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,38 @@
 import React from "react";
-
+ import {useState,useEffect} from "react"
+ import "./Nav.css"
 const Home =()=>{
+  const [data ,setdata] = useState([])
+
+  useEffect(()=>{
+  fetch("https://dummyjson.com/products")
+  .then(response=>response.json()).then((res)=>setdata(res.products)).catch(()=>{console.log("data not found")})
+   
+   
+
+  },[])
+
+  {
+    console.log(data)
+  }
   return (
-    <h1> Home</h1>
+    <>
+  
+      <div className="box">
+      {
+      data.map((data)=>(
+        // console.log(data.brand)
+      
+        <div className="data">
+    <h3>  {data.brand}</h3>
+     <img src={data.thumbnail} alt="error"/>
+     
+        </div>
+      ))
+    }
+      </div>
+  
+    </>
   )
 }
 export default  Home;
